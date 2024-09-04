5b9ff2a7b79c3a15e93c5e357fa9c14b3c113f01
hammad <hamidhammad124@gmail.com>
2024-09-04 18:01:17 +0500

    structure of folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Homepage from './landing_page/home/Homepage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Homepage />
+  </StrictMode>
 )
