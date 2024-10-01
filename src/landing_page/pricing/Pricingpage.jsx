d3370a37d3b72f92eac29c8dcd7af401f0877faa
hammad <hamidhammad124@gmail.com>
2024-10-01 14:31:09 +0500

    pricing complete

--- src/landing_page/pricing/Pricingpage.jsx
+++ src/landing_page/pricing/Pricingpage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Brockrage from "./Brokerage";
 import Hero from "./Hero";
+import OpenAccount from "../OpenAccount"
 
 function Pricing() {
     return ( <div>
         
         <Hero/>
+        <OpenAccount/>
         <Brockrage/>
     </div> );
 }
